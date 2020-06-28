315d307a9c9f9f9f27548dfe352857d7fd6e16cf
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-06-28 11:42:05 +0200

    find problems

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -22,25 +22,23 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter App"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text("Chart"),
-              elevation: 5,
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text("Chart"),
+                elevation: 5,
               ),
-          ),            
-          ),
-          Card(
-            child: Text("List of TX"),
-          ),
-        ],
-      ),
-    );
+            ),
+            Card(
+              child: Text("List of TX"),
+            ),
+          ],
+        ));
   }
 }
