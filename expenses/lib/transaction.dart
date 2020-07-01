97c7c2651d795270c42e418adf17193ed0bda220
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-07-01 18:04:36 +0200

    transaction

--- expenses/lib/transaction.dart
+++ expenses/lib/transaction.dart
@@ -1,8 +1,8 @@
 class Transaction {
-  String id;
-  String title;
-  double amount;
-  DateTime date;
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
   //{}for named arguments
-  Transaction({});
+  Transaction({this.id, this.title, this.amount, this.date});
 }
