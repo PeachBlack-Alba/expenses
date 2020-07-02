d46dcada98a59682c6390a7aaaf41df5814b833d
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-07-02 20:07:48 +0200

    now launches problem with two materialApp

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -8,12 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter App',
+      home: MyHomePage(),
     );
   }
 }
