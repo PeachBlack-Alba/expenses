f0b21d633ffa2ce09440807472c2a62c901cccef
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-07-28 15:04:31 +0200

    fonts

--- expenses/lib/widgets/transaction_list.dart
+++ expenses/lib/widgets/transaction_list.dart
@@ -40,7 +40,7 @@ class TransactionList extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
